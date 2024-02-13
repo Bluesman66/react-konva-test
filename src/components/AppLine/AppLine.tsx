e8457387a46d32e5be8f28d5f03c1ceff7fda232
Aleksandr Paklin <paklin@profiterole.group>
2024-02-13 16:44:49 +0500

    line with tooltip

--- src/components/AppLine/AppLine.tsx
+++ src/components/AppLine/AppLine.tsx
@@ -24,12 +24,11 @@ const AppLine = ({
   const pointEndRef = useRef<Konva.Circle>(null);
   const tooltipRef = useRef<Konva.Text>(null);
 
-  // handlers
-  const handleDragMove = (e: Konva.KonvaEventObject<DragEvent>) => {
-    const { x, y } = e.target.attrs;
+  // methods
+  const dragMove = (x: number, y: number, text: string) => {
     if (tooltipRef.current) {
       tooltipRef.current.position({ x: x + 20, y: y + 20 });
-      tooltipRef.current.text("Red Circle");
+      tooltipRef.current.text(text);
       tooltipRef.current.show();
     }
 
@@ -39,8 +38,39 @@ const AppLine = ({
       toNumber(pointEndRef.current?.x()),
       toNumber(pointEndRef.current?.y()),
     ]);
+  };
 
+  const showBeginAngle = (x: number, y: number) => {
+    const dx = x - toNumber(pointEndRef.current?.x());
+    const dy = y - toNumber(pointEndRef.current?.y());
+    const angle = (Math.atan(dy / dx) * 180) / Math.PI;
+    dragMove(x, y, angle.toFixed(2).toString());
+  };
+
+  const showEndAngle = (x: number, y: number) => {
+    const dx = x - toNumber(pointBeginRef.current?.x());
+    const dy = y - toNumber(pointBeginRef.current?.y());
+    const angle = (Math.atan(-dy / dx) * 180) / Math.PI;
+    dragMove(x, y, angle.toFixed(2).toString());
+  };
+
+  const hideTooltip = () => {
+    if (tooltipRef.current) {
+      tooltipRef.current.hide();
+    }
+  };
+
+  // handlers
+  const handleDragMoveBegin = (e: Konva.KonvaEventObject<DragEvent>) => {
     onDragMove?.(e);
+    const { x, y } = e.target.attrs;
+    showBeginAngle(x, y);
+  };
+
+  const handleDragMoveEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
+    onDragMove?.(e);
+    const { x, y } = e.target.attrs;
+    showEndAngle(x, y);
   };
 
   const handleMouseOver = () => {
@@ -52,11 +82,20 @@ const AppLine = ({
   };
 
   const handleDragDrop = (e: Konva.KonvaEventObject<DragEvent>) => {
-    if (tooltipRef.current) {
-      tooltipRef.current.hide();
-    }
-
     onDragDrop?.(e);
+    hideTooltip();
+  };
+
+  const handleMouseDownBegin = () => {
+    showBeginAngle(toNumber(pointBeginRef.current?.x()), toNumber(pointBeginRef.current?.y()));
+  };
+
+  const handleMouseDownEnd = () => {
+    showEndAngle(toNumber(pointEndRef.current?.x()), toNumber(pointEndRef.current?.y()));
+  };
+
+  const handleMouseUp = () => {
+    hideTooltip();
   };
 
   if (line.length !== 4) {
@@ -74,9 +113,11 @@ const AppLine = ({
         stroke={pointColor}
         onDragStart={onDragBegin}
         onDragEnd={handleDragDrop}
-        onDragMove={handleDragMove}
+        onDragMove={handleDragMoveBegin}
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
+        onMouseDown={handleMouseDownBegin}
+        onMouseUp={handleMouseUp}
       />
       <Circle
         ref={pointEndRef}
@@ -87,9 +128,11 @@ const AppLine = ({
         stroke={pointColor}
         onDragStart={onDragBegin}
         onDragEnd={handleDragDrop}
-        onDragMove={handleDragMove}
+        onDragMove={handleDragMoveEnd}
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
+        onMouseDown={handleMouseDownEnd}
+        onMouseUp={handleMouseUp}
       />
       <Line points={[...line]} stroke={lineColor} strokeWidth={lineWidth} lineCap="round" lineJoin="round" />
       <Text ref={tooltipRef} fontSize={15} visible={false} />
--- src/components/HumanEditor/HumanEditor.tsx
+++ src/components/HumanEditor/HumanEditor.tsx
@@ -1,25 +1,17 @@
-import { useRef } from "react";
-import Konva from "konva";
 import { Layer, Stage } from "react-konva";
 
 import { HumanEditorProps } from "./HumanEditor.props";
 import AppLine from "../AppLine/AppLine";
 
 const HumanEditor = ({ width, height }: HumanEditorProps) => {
-  //refs
-  const stageRef = useRef<Konva.Stage>(null);
-  const shapesLayerRef = useRef<Konva.Layer>(null);
-  const tooltipLayerRef = useRef<Konva.Layer>(null);
-
   return (
-    <Stage ref={stageRef} width={width} height={height}>
-      <Layer ref={shapesLayerRef}>
+    <Stage width={width} height={height}>
+      <Layer>
         <AppLine initialCoords={[100, 100, 200, 100]} />
         <AppLine initialCoords={[100, 200, 200, 200]} />
         <AppLine initialCoords={[100, 300, 200, 300]} />
         <AppLine initialCoords={[100, 400, 200, 400]} />
       </Layer>
-      <Layer ref={tooltipLayerRef}></Layer>
     </Stage>
   );
 };
